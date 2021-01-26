5c88e78234a2a2a3bcd62268f8828e0b01c33e1a
Simon Bubendorf <simon@bubendorf-pluess.ch>
2021-01-26 20:48:47 +0100

    Add Anchor Pane Example with Buttons anchored to the right bottom corner

---
+++ src/main/java/ch/rusi/CornerButtonsEx.java
@@ -0,0 +1,43 @@
+package ch.rusi;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class CornerButtonsEx extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        initUI(stage);
+    }
+
+    private void initUI(Stage stage) {
+
+        AnchorPane root = new AnchorPane();
+
+        Button buttonOk = new Button("OK");
+        Button buttonClose = new Button("Close");
+        HBox box = new HBox(5, buttonOk, buttonClose);
+
+        root.getChildren().addAll(box);
+
+        AnchorPane.setRightAnchor(box, 10d);
+        AnchorPane.setBottomAnchor(box, 10d);
+
+        Scene scene = new Scene(root, 300, 200);
+
+        stage.setTitle("Corner buttons");
+        stage.setScene(scene);
+        stage.setMinWidth(300);
+        stage.setMinHeight(300);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
