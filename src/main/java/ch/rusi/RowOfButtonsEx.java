4a528d158ab8427691b3b0df5e1258a7e8cab0a2
Simon Bubendorf <simon@bubendorf-pluess.ch>
2021-01-26 20:10:05 +0100

    Add Row of Buttons Example with HBox

---
+++ src/main/java/ch/rusi/RowOfButtonsEx.java
@@ -0,0 +1,43 @@
+package ch.rusi;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class RowOfButtonsEx extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        initUI(stage);
+    }
+
+    private void initUI(Stage stage) {
+
+
+        HBox root = new HBox(5 ); // Horizontal gap between buttons
+        root.setPadding(new Insets(10));    // Distance of buttons from window border
+        root.setAlignment(Pos.BASELINE_RIGHT);
+
+        Button prevBtn = new Button("Previous");
+        Button nextBtn = new Button("Next");
+        Button cancBtn = new Button("Cancel");
+        Button helpBtn = new Button("Help");
+
+        root.getChildren().addAll(prevBtn, nextBtn, cancBtn, helpBtn);
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Row of buttons");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
